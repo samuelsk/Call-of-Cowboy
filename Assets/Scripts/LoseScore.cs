d725c74481022e242ff0047340d59dc97497dc89
samuelsk <Samuel@Samuels-MBP.home>
2015-09-07 13:48:26 -0300

    Atualização para a loja
    - Anúncios adicionados
    - Backgrounds da “MainScreen” e da “LoseScren” alterados

--- Assets/Scripts/LoseScore.cs
+++ Assets/Scripts/LoseScore.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Advertisements;
 
 public class LoseScore : MonoBehaviour {
 
@@ -8,8 +9,12 @@ public class LoseScore : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//Mostra o anúncio.
+		if (Advertisement.isReady ()) {
+			Advertisement.Show();
+		}
 		//scoreText.text = "Score: "+GameControllerCowboy.score;
-		GetComponent<Text> ().text = "Score: " + GameControllerCowboy.score;
+		GetComponent<Text> ().text = "Score:\n" + GameControllerCowboy.score;
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Advertisements;
 
-public class MenuScript : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+	//Permite o uso de anúncios do UnityAds.
+		Advertisement.Initialize ("71200");
 	}
 	
 	// Update is called once per frame
