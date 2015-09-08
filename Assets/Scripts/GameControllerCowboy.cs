92f7d9345204f3d669df4148c92ce74e5b4ef9d7
LukeLeal <lukelealm@gmail.com>
2015-09-08 17:04:42 -0300

    Bugfix

--- Assets/Scripts/GameControllerCowboy.cs
+++ Assets/Scripts/GameControllerCowboy.cs
@@ -7,8 +7,8 @@ public class GameControllerCowboy : MonoBehaviour {
 	public Camera cam;
 	public GameObject enemy;
 	public int life = 3;
-	public float spawnSpeedMin;
-	public float spawnSpeedMax;
+	private float spawnSpeedMin = 0.5f;
+	private float spawnSpeedMax = 1f;
 	public Text lifeText;
 	public Text scoreText;
 	//public int mode = 1;
@@ -45,19 +45,20 @@ public class GameControllerCowboy : MonoBehaviour {
 //					life++;
 //					lifeText.text = "Life: " + life;
 				}
-				if(score == 4000){
-					spawnSpeedMin = 0.4f;
-					spawnSpeedMax = 0.6f;
-				} else if (score == 8000){
-					spawnSpeedMin = 0.3f;
-					spawnSpeedMax = 0.5f;
-				}
+//				if(score == 4000){
+//					spawnSpeedMin = 0.3f;
+//					spawnSpeedMax = 0.5f;
+//				} else if (score == 1000){
+//					spawnSpeedMin = 0.01f;
+//					spawnSpeedMax = 0.1f;
+//				}
 				Debug.Log(modifier);
 			}
 			Vector3 spawnPosition = new Vector3 (Random.Range (-maxWidth, maxWidth), transform.position.y, 0.0f);
 			Quaternion spawnRotation = Quaternion.identity; // Sem rotação
 			enemy.GetComponent<EnemyScript>().speed = 4 + modifier * 0.5f;
 			Instantiate (enemy, spawnPosition, spawnRotation);
+			Debug.Log(spawnSpeedMax);
 			yield return new WaitForSeconds(Random.Range (spawnSpeedMin, spawnSpeedMax));
 		}
 	}
@@ -67,6 +68,15 @@ public class GameControllerCowboy : MonoBehaviour {
 		if (score != lastScore) {
 			lastScore = score;
 			scoreText.text = score.ToString();
+		}
+
+		if(score == 4000){
+			spawnSpeedMin = 0.4f;
+			spawnSpeedMax = 0.6f;
+		} else if (score == 8000){
+			//Debug.Log("ENTROU");
+			spawnSpeedMin = 0.25f;
+			spawnSpeedMax = 0.45f;
 		}
 	}
 
